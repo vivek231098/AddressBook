fa451252f7ac9a283ebe454619ac6c973db7bf89
vivek231098 <menonvivek196@gmail.com>
2020-08-26 20:47:14 +0530

    Pojo class features

---
+++ person.java
@@ -0,0 +1,10 @@
+package addressBook;
+/*pojoclass for Person */
+public class Person {
+	public String firstName;
+	public String lastName;
+	public String city;
+	public String state;
+	public long zip;
+	public String phoneNumber;
+	
